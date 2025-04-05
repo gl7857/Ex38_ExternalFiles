cb3dfd257019ce3f6a086dbc531555431d20e507
gl7857 <gl7857@bs.amalnet.k12.il>
2025-04-05 12:52:37 +0300

    Ex38_ExternalFiles

--- app/src/main/java/com/example/ex38_externalfiles/Credits.java
+++ app/src/main/java/com/example/ex38_externalfiles/Credits.java
@@ -14,7 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
  *
  * @author      Gali Lavi <gl7857@bs.amalnet.k12.il>
  * @version     1.0
- * @since       03/04/2025
+ * @since       3/04/2025
  *
  * short description:
  *        This activity enables edge-to-edge display and shows a credits screen.
